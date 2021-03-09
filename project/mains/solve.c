b7e1e8a22ebdc43fcf01be56ada9dbb237452d7c
harmankharoud <singh.harman.dev@gmail.com>
2021-03-09 09:31:15 -0800

    --refactored, renamed and changed indentation in solve.c

--- project/mains/solve.c
+++ project/mains/solve.c
@@ -19,217 +19,210 @@
 #include "maze.c"
 #include "breadthfirst.c"
 
-void solve(char *arguments[]){
+void solve(char *inputFile, char *outputFile){
+  // Load Image 
+  printf("Loading Image \n");
+  
+  int width,height,n;
+  unsigned char* image = stbi_load(inputFile, &width, &height, &n, 1);
 
-    clock_t c_i1, c_i2;
+  // rgb is now three bytes per pixel, width*height size. Or NULL if load failed.    
+  if (image == 0) {
+    printf("No image found or proccessed :( \n");
+    return;
+  }
+  
 
-    Image_Dimentions im;
-    // Load Image 
-    printf("Loading Image \n");
-    char *input_file = arguments[1]; // input image to be solve
-    char *output_file = arguments[2]; // output solved image.
+  printf("Creating Maze \n");
+  //get an instance of get time function
+  char* t0 = loadTime();
+  
+
+  printf("Maze started creating Nodes at %s:", t0);
+
+  clock_t startTime, endTime;
+
+  startTime = clock();
+
+  Image_Dimentions im;
+  im.height = width;
+  im.width = height;
+  int imageSize = (width*height);
+
+  //This is to dynamically intiliaze array so that array size/memory can be passed from image size
+  Array imagePixels;
+  initArray(&imagePixels, imageSize);
+
+  //This need to be intilized here so we can realloc more memory
+  Nodes *routeNodes = (Nodes *) malloc(sizeof(Nodes));
+
+  Nodes startNode;
+  initStruct(&startNode, 1);
+
+  Nodes endNode;
+  initStruct(&endNode, 1);
+
+  for(int i = 0; i < imageSize; i++){
+    imagePixels.array[i] = image[i];
+  }
+
+  //send maze class all the info to read nodes and image size
+  //getting a pointer back to the new address assigned to the pathNode
+  //its either same pointer or realloc changed the address in memory
+  //either way it will return the proper pointer to address
+  Nodes *pathNode = Maze(im, imagePixels.array, routeNodes, &startNode, &endNode);
+
+  //return pathNode;
+
+  if(pathNode != NULL){
+    //get the solved path as a pointer to the localally created array in the file
+    int (*solvedPath)[2] = solver(im, pathNode, startNode, endNode);
+
+    char* tMazeSolved = loadTime();
+    printf("Image Solved at: \n");
+    printf("%s", tMazeSolved);
+
+    //get the size of path
+    int pathSize = getPathSize(-1);
+
+    int resultImageSize = 3*imageSize;
     
-    int x,y,n;
-    unsigned char* image = stbi_load(input_file, &x, &y, &n, 1);
-    // rgb is now three bytes per pixel, width*height size. Or NULL if load failed.
-    
-    if (image == 0) {
-        printf("No image found or proccessed :( \n");
-        return;
-    }
-    
+    unsigned char *resultImage = malloc(resultImageSize * sizeof(unsigned char));
 
-    printf("Creating Maze \n");
-    //get an instance of get time function
-    char* t0 = loadTime();
-    
-
-    printf("Maze started creating Nodes at: \n");
-    printf("%s", t0);
-    c_i1 = clock();
-
-    im.height = x;
-    im.width = y;
-    int image_size = (x*y);
-
-    //This is to dynamically intiliaze array so that array size/memory can be passed from image size
-    Array image_pixels;
-    initArray(&image_pixels, image_size);
-
-    //This need to be intilized here so we can realloc more memory
-    Nodes *route_nodes = (Nodes *) malloc(sizeof(Nodes));
-
-    Nodes start_node;
-    initStruct(&start_node, 1);
-
-    Nodes end_node;
-    initStruct(&end_node, 1);
-
-    for(int i = 0; i < image_size; i++){
-        image_pixels.array[i] = image[i];
-    }
-
-    //send maze class all the info to read nodes and image size
-    //getting a pointer back to the new address assigned to the path_node
-    //its either same pointer or realloc changed the address in memory
-    //either way it will return the proper pointer to address
-    Nodes *path_node = Maze(im, image_pixels.array, route_nodes, &start_node, &end_node);
-
-    //return path_node;
-
-    if(path_node != NULL){
-        //get the solved path as a pointer to the localally created array in the file
-       int (*solved_path)[2] = solver(im, path_node, start_node, end_node);
-
-       char* t_mazeSolved = loadTime();
-       printf("Image Solved at: \n");
-       printf("%s", t_mazeSolved);
-
-       //get the size of path
-       int path_size = getPathSize(-1);
-
-       int write_image_size = 3*image_size;
-       
-       unsigned char *result_image = malloc(write_image_size * sizeof(unsigned char));
-
-       int track = 0;
-       int isFirst = 0;
-       for(int i = 0; i < image_size; i++){
-            if(image_pixels.array[i] == 255){
-                //add to three more bytes, its a png
-                if(isFirst == 0){
-                    isFirst = 1;
-                    int i_value = 0;
-                    while(i_value < 3){
-                        if(i_value == 0){
-                            result_image[i_value + track] = 255;
-                        } else{
-                            result_image[i_value + track] = 0;
-                        }
-                        i_value++;
-                        if(i_value == 3){
-                            track += 3;
-                        }
-                    }
-                } else{
-                    int i_value = 0;
-                    while(i_value < 3){
-                        result_image[i_value + track] = 255;
-                        i_value++;
-                        if(i_value == 3){
-                            track += 3;
-                        }
-                    }
-                }                
+    int track = 0;
+    int isFirst = 0;
+    for(int i = 0; i < imageSize; i++){
+      if(imagePixels.array[i] == 255){
+          //add to three more bytes, its a png
+        if(isFirst == 0){
+          isFirst = 1;
+          int rgbValue = 0;
+          while(rgbValue < 3){
+            if(rgbValue == 0){
+              resultImage[rgbValue + track] = 255;
+            } else{
+              resultImage[rgbValue + track] = 0;
             }
-
-            if(image_pixels.array[i] == 0){
-                int i_value = 0;
-                while(i_value < 3){
-                    result_image[i_value + track] = 0;
-                    i_value++;
-                    if(i_value == 3){
-                        track += 3;
-                    } 
-                }
+            rgbValue++;
+            if(rgbValue == 3){
+              track += 3;
             }
-
-       }
-
-    //free the mem reserved for path
-        
-        for(int i = 0; i < path_size; i++){
-            int min;
-            int max;
-
-            if(solved_path[i][0] == solved_path[i + 1][0]){
-                if(solved_path[i][1] < solved_path[i + 1][1]){
-                    min = solved_path[i][1];
-                    max = solved_path[i + 1][1];
-                } else{
-                    min = solved_path[i + 1][1];
-                    max = solved_path[i][1];
-                }
-
-                for(int j = min; j < max; j++){
-                    int k = j + (solved_path[i][0] * y);
-                    int i_value = k * 3;
-                    int j_value = 0;
-                    while(j_value < 3){
-                        if(j_value == 0){
-                            result_image[k * 3] = 255;
-                        } else{
-                            result_image[i_value] = 0;
-                        }
-                        i_value++;
-                        j_value++;
-                    }
-                }
-            } else if(solved_path[i][1] == solved_path[i + 1][1]){
-                if(solved_path[i][0] < solved_path[i + 1][0]){
-                    min = solved_path[i][0];
-                    max = solved_path[i + 1][0] + 1;
-                } else{
-                    min = solved_path[i + 1][0];
-                    max = solved_path[i][0] + 1;
-                }
-
-                for(int j = min; j < max; j++){
-                    int k = (j * y) + solved_path[i][1];
-                    int i_value = k * 3;
-                    int j_value = 0;
-                    while(j_value < 3){
-                        if(j_value == 0){
-                            result_image[i_value] = 255;
-                        } else{
-                            result_image[i_value] = 0;
-                        }
-                        i_value++;
-                        j_value++;
-                    }
-                }
-            }
-        } 
-
-        STBIW_ASSERT(result_image);
-        int image_solve = stbi_write_png(output_file, x, y, 3, result_image, x*3);
-        if (image_solve == 1){
-            //free the memory after image succesfully created.
-            freeArray(&image_pixels);
-            stbi_image_free(image); 
-            free(result_image);
-
-            //create an instance of get time function
-            char* t1 = loadTime();
-            printf("Maze solved at: \n");
-            printf("%s", t1);
-            c_i2 = clock();
-            //int total = t1 -t0;
-            float diff = ((float)(c_i2 - c_i1) / 1000000.0F ) * 1000;
-            printf ("Time elapsed: %f \n", diff); 
-
-            
-            //free(&solved_path);
+          }
         } else{
-            printf("Failed to write file.");
-        }
- 
+          int rgbValue = 0;
+          while(rgbValue < 3){
+            resultImage[rgbValue + track] = 255;
+            rgbValue++;
+            if(rgbValue == 3){
+              track += 3;
+            }
+          }
+        }                
+      }
 
+      if(imagePixels.array[i] == 0){
+        int rgbValue = 0;
+        while(rgbValue < 3){
+          resultImage[rgbValue + track] = 0;
+          rgbValue++;
+          if(rgbValue == 3){
+            track += 3;
+          } 
+        }
+      }
     }
+
+    //free the mem reserved for path        
+    for(int i = 0; i < pathSize; i++){
+      int min;
+      int max;
+      if(solvedPath[i][0] == solvedPath[i + 1][0]){
+        if(solvedPath[i][1] < solvedPath[i + 1][1]){
+          min = solvedPath[i][1];
+          max = solvedPath[i + 1][1];
+        } else{
+          min = solvedPath[i + 1][1];
+          max = solvedPath[i][1];
+        }
+
+        for(int j = min; j < max; j++){
+          int k = j + (solvedPath[i][0] * height);
+          int offsetValue = k * 3;
+          int rgbValue = 0;
+          while(rgbValue < 3){
+            if(rgbValue == 0){
+              resultImage[k * 3] = 255;
+            } else{
+              resultImage[offsetValue] = 0;
+            }
+            offsetValue++;
+            rgbValue++;
+          }
+        }
+      } else if(solvedPath[i][1] == solvedPath[i + 1][1]){
+        if(solvedPath[i][0] < solvedPath[i + 1][0]){
+          min = solvedPath[i][0];
+          max = solvedPath[i + 1][0] + 1;
+        } else{
+          min = solvedPath[i + 1][0];
+          max = solvedPath[i][0] + 1;
+        }
+
+        for(int j = min; j < max; j++){
+          int k = (j * height) + solvedPath[i][1];
+          int offsetValue = k * 3;
+          int rgbValue = 0;
+          while(rgbValue < 3){
+            if(rgbValue == 0){
+              resultImage[offsetValue] = 255;
+            } else{
+              resultImage[offsetValue] = 0;
+            }
+            offsetValue++;
+            rgbValue++;
+          }
+        }
+      }
+    } 
+
+    STBIW_ASSERT(resultImage);
+    int imageSolved = stbi_write_png(outputFile, width, height, 3, resultImage, width*3);
+    if (imageSolved == 1){
+      //free the memory after image succesfully created.
+      freeArray(&imagePixels);
+      stbi_image_free(image); 
+      free(resultImage);
+
+      //create an instance of get time function
+      char* t1 = loadTime();
+      printf("Maze solved at: \n");
+      printf("%s", t1);
+      endTime = clock();
+      float diff = ((float)(endTime - startTime) / 1000000.0F ) * 1000;
+      printf ("Time elapsed: %f \n", diff); 
+
+      
+      //free(&solvedPath);
+    } else{
+      printf("Failed to write file.");
+    }
+  }
 }
 
 
 int main(int argc, char *argv[]) 
 {
 
-    //create a struct to get back start_node, end_node, path_node, image_size
-    //return all the values from solve function.
-    //include selected algoritm from cmd line here inside this function.
-    //including inside main funtion is bad but temp solution for now.
+  //create a struct to get back startNode, endNode, pathNode, imageSize
+  //return all the values from solve function.
+  //include selected algoritm from cmd line here inside this function.
+  //including inside main funtion is bad but temp solution for now.
 
-   //Nodes *path_node = solve(argv);
-    solve(argv);   
+  //Nodes *pathNode = solve(argv);
 
-    return 0;
+  char *inputFile = argv[1]; // input image to be solve
+  char *outputFile = argv[2]; // output solved image.
+  solve(inputFile, outputFile); 
+  return 0;
 }
  
