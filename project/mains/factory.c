1f208b6bc7b61e989a39e4b49eb2a544473d02d3
harmankharoud <singh.harman.dev@gmail.com>
2021-03-09 11:30:37 -0800

    --renamed bfs class --improved logic in factory class

--- project/mains/factory.c
+++ project/mains/factory.c
@@ -4,31 +4,23 @@
 #include "breadthfirst.c"
 
 int (*factory(char *selected, Image_Dimentions im, Nodes *routeNodes, Nodes startNode, Nodes endNode))[2]{
-  //create cases to make it easier to load
-  const static char *listOfAlgorithms[4] = {"breadthfirst", "depthfirst", "astar", "leftturn"};
-  char selection;
-  for (int i = 0; i < 4; i++){
-    if(listOfAlgorithms[i][0] == selected[0]) {
-      selection = listOfAlgorithms[i][0];
-    }
-  }
   
   int (*path)[2] = malloc(sizeof(int[1][2]));
-  switch(selection) {
+  switch(selected[0]) {
   case 'b' :
-    path = solver(im, routeNodes, startNode, endNode);
+    path = bfs(im, routeNodes, startNode, endNode);
     break;
-  case 1 :
+  case 'd' :
     // depthFirst.
     break;
-  case 2 :
+  case 'a' :
     // astar
     break;
-  case 3 :
+  case 'l' :
     // leftturn
     break;
   default :
-    path = solver(im, routeNodes, startNode, endNode);
+    path = bfs(im, routeNodes, startNode, endNode);
   }
 
    return path;
